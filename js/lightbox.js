902fbef22c2f73da3a7243187fedab2a594bdd04
Chris Balin <chris@inventables.com>
2015-09-16 15:02:31 -0500

    Don't group hero images in lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,17 +1,22 @@
 // Expand instruction images and embedded images from textile into colorbox, keeping gallery scoped to steps
+$("body").find("img").each(function() {
+  $(this).colorbox({
+    title:  function() {
+      var url = $(this).data('original') || $(this).attr('src');
+      return '<a href="' + url + '" target="_blank">Open in a new window</a>';
+    },
+    href: function() {
+      return $(this).data('original') || $(this).attr('src');
+    },
+    maxWidth: "100%",
+    maxHeight: "100%"
+  });
+});
+
 var galleryIndex = 1;
-$(".row").each(function() {
+$(".image-row").each(function() {
   $(this).find("img").each(function() {
     $(this).colorbox({
-      title:  function() {
-        var url = $(this).data('original') || $(this).attr('src');
-        return '<a href="' + url + '" target="_blank">Open in a new window</a>';
-      },
-      href: function() {
-        return $(this).data('original') || $(this).attr('src');
-      },
-      maxWidth: "100%",
-      maxHeight: "100%",
       rel: "gallery-step" + galleryIndex
     });
   });
